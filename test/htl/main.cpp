43cb569b58b8b4d1f963b10db8dfaac329895084
John Hunter Kohler <jhunterkohler@gmail.com>
2022-12-08 02:31:26 -0500

    Add type coersion to ascii test

--- test/htl/main.cpp
+++ test/htl/main.cpp
@@ -1,16 +1,7 @@
-#include <clocale>
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv)
 {
-    std::cout << "LC_ALL: " << std::setlocale(LC_ALL, nullptr) << "\n";
-    std::cout << "LC_COLLATE: " << std::setlocale(LC_COLLATE, nullptr) << "\n";
-    std::cout << "LC_CTYPE: " << std::setlocale(LC_CTYPE, nullptr) << "\n";
-    std::cout << "LC_MONETARY: " << std::setlocale(LC_MONETARY, nullptr)
-              << "\n";
-    std::cout << "LC_NUMERIC: " << std::setlocale(LC_NUMERIC, nullptr) << "\n";
-    std::cout << "LC_TIME: " << std::setlocale(LC_TIME, nullptr) << "\n";
-    return 0;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- test/htl/test_ascii.cpp
+++ test/htl/test_ascii.cpp
@@ -6,10 +6,11 @@ namespace htl::test {
 
 constexpr std::ranges::iota_view ascii_view{ 0, 128 };
 
+template <class T>
 auto assert_same_under_ascii(auto f1, auto f2)
 {
     for (char n: ascii_view) {
-        ASSERT_EQ(f1(n), f2(n));
+        ASSERT_EQ(static_cast<T>(f1(n)), static_cast<T>(f2(n)));
     }
 }
 
@@ -23,72 +24,72 @@ TEST(AsciiTest, AsciiIsAscii)
 
 TEST(AsciiTest, AsciiIsDigit)
 {
-    assert_same_under_ascii(ascii_isdigit, isdigit);
+    assert_same_under_ascii<bool>(ascii_isdigit, isdigit);
 }
 
 TEST(AsciiTest, AsciiIsLower)
 {
-    assert_same_under_ascii(ascii_islower, islower);
+    assert_same_under_ascii<bool>(ascii_islower, islower);
 }
 
 TEST(AsciiTest, AsciiIsUpper)
 {
-    assert_same_under_ascii(ascii_isupper, isupper);
+    assert_same_under_ascii<bool>(ascii_isupper, isupper);
 }
 
 TEST(AsciiTest, AsciiIsGraph)
 {
-    assert_same_under_ascii(ascii_isgraph, isgraph);
+    assert_same_under_ascii<bool>(ascii_isgraph, isgraph);
 }
 
 TEST(AsciiTest, AsciiIsPrint)
 {
-    assert_same_under_ascii(ascii_isprint, isprint);
+    assert_same_under_ascii<bool>(ascii_isprint, isprint);
 }
 
 TEST(AsciiTest, AsciiIsAlnum)
 {
-    assert_same_under_ascii(ascii_isalnum, isalnum);
+    assert_same_under_ascii<bool>(ascii_isalnum, isalnum);
 }
 
 TEST(AsciiTest, AsciiIsAlpha)
 {
-    assert_same_under_ascii(ascii_isalpha, isalpha);
+    assert_same_under_ascii<bool>(ascii_isalpha, isalpha);
 }
 
 TEST(AsciiTest, AsciiIsBlank)
 {
-    assert_same_under_ascii(ascii_isblank, isblank);
+    assert_same_under_ascii<bool>(ascii_isblank, isblank);
 }
 
 TEST(AsciiTest, AsciiIsCntrl)
 {
-    assert_same_under_ascii(ascii_iscntrl, iscntrl);
+    assert_same_under_ascii<bool>(ascii_iscntrl, iscntrl);
 }
 
 TEST(AsciiTest, AsciiIsPunct)
 {
-    assert_same_under_ascii(ascii_ispunct, ispunct);
+    assert_same_under_ascii<bool>(ascii_ispunct, ispunct);
 }
 
 TEST(AsciiTest, AsciiIsSpace)
 {
-    assert_same_under_ascii(ascii_isspace, isspace);
+    assert_same_under_ascii<bool>(ascii_isspace, isspace);
 }
 
 TEST(AsciiTest, AsciiIsXdigit)
 {
-    assert_same_under_ascii(ascii_isxdigit, isxdigit);
+    assert_same_under_ascii<bool>(ascii_isxdigit, isxdigit);
 }
 
 TEST(AsciiTest, AsciiToLower)
 {
-    assert_same_under_ascii(ascii_tolower, tolower);
+    assert_same_under_ascii<char>(ascii_tolower, tolower);
 }
 
 TEST(AsciiTest, AsciiToUpper)
 {
-    assert_same_under_ascii(ascii_toupper, toupper);
+    assert_same_under_ascii<char>(ascii_toupper, toupper);
 }
 
 } // namespace htl::test
